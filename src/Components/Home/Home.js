ab3088829dcf315323f2add22faf0ae5f976f532
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-28 11:26:00 +0600

    ui ok

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,20 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import Fakedata from "../../Fakedata/fakedata.json";
+import Product from "../Product/Product";
 import "./Home.css";
-const Home = () => {
+function Home() {
   return (
     <div className="Home-container">
       <div className="product-container">
-        <ul>
-          {Fakedata.map((product) => (
-            <li>{product.name}</li>
-          ))}
-        </ul>
+        {Fakedata.map((product) => (
+          <Product product={product}></Product>
+        ))}
       </div>
       <div className="cart-container">
         <h3>this is cart </h3>
       </div>
     </div>
   );
-};
+}
 export default Home;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Product = (props) => {
+  console.log(props.product.name);
+  return (
+    <div>
+      <div>
+        <img src={props.product.img} alt="" />
+      </div>
+      <div>
+        <h1>{props.product.name} </h1>
+      </div>
+    </div>
+  );
+};
+export default Product;
