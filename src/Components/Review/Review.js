5fd7617753d3bf2c92c347b5f50218357a051b77
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-08 05:27:20 +0600

    ok review

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { getDatabaseCart } from "../../Utilities/databaseManager";
+import {
+  getDatabaseCart,
+  removeFromDatabaseCart
+} from "../../Utilities/databaseManager";
 import Fakedata from "../../Fakedata/fakedata.json";
 import ReviewItem from "../../ReviewItem/ReviewItem";
+import Cart from "../Cart/Cart";
 const Review = () => {
   const [cart, setCart] = useState([]);
   const removeProduct = (name) => {
     console.log("remove clicked", name);
     const newCart = cart.filter((product) => product.name !== name);
     setCart(newCart);
+    removeFromDatabaseCart(name);
   };
   useEffect(() => {
     const saveCart = getDatabaseCart();
@@ -21,13 +26,17 @@ const Review = () => {
     setCart(cartProduct);
   });
   return (
-    <div>
-      <h1>cart items :{cart.length}</h1>
-      {cart.map((product) => (
-        <ReviewItem product={product} removeProduct={removeProduct}>
-          {" "}
-        </ReviewItem>
-      ))}
+    <div className="Home-container">
+      <div className="product-container">
+        {cart.map((product) => (
+          <ReviewItem product={product} removeProduct={removeProduct}>
+            {" "}
+          </ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
     </div>
   );
 };
