d481d63f50fed9aad0acfe0981916de60b0663ef
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-17 05:29:26 +0600

    pop up done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,15 +2,32 @@ import React from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseconfig from "./firebase.config";
+firebase.initializeApp(firebaseconfig);
 
 const Login = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn = () => {
-    firebase.auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-  console.log(result)
-  });
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+
+        var token = credential.accessToken;
+
+        var user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        var email = error.email;
+
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email);
+      });
+  };
 
   return (
     <div>
