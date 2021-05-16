ab572044964a1d28e348098b5f9f1e7852c75077
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-17 04:40:36 +0600

    ok some thing

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,8 +1,20 @@
 import React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseconfig from "./firebase.config";
+
 const Login = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+  console.log(result)
+  });
+
   return (
     <div>
-      
+      <button onClick={handleSignIn}>Sign In </button>
     </div>
   );
 };
