b0212b42cb3ac523bdb08e354eefcfa9284560a7
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-17 06:02:51 +0600

    context

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ import NotFound from "./Components/NotFound/NotFound";
 import ProductDetail from "./Components/ProductDetail/ProductDetail";
 import Shipment from "./Components/shipment/shipment";
 import Login from "./Components/Login/Login";
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
 
 export default function App() {
+  const [loggedInUser, setloggedInUser] = useState({});
   return (
-    <div className="App">
+    <UserContext.Provider value={[loggedInUser,setloggedInUser]}>
       <Header />
       <Router>
         <Switch>
@@ -49,6 +53,6 @@ export default function App() {
           </Route>
         </Switch>
       </Router>
-    </div>
+    </UserContext.Provider>
   );
 }
