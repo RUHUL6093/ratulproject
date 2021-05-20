ae1bdac1c8d74b55e40ddfd9eb25ede8e1f3caed
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-20 22:15:16 +0600

    ok problem

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,20 +1,22 @@
 import React from "react";
-// import TweenLite from 'gsap/TweenLite'
+
 import Fakedata from "../../Fakedata/fakedata.json";
+
 const Admin = () => {
   const handleAddProduct = () => {
-    fetch('https://5562b.sse.codesandbox.io/addProducts',{
-      method:'POST',
-      headers:{
-        'Content-Type': 'application/json'
+    fetch("https://5562b.sse.codesandbox.io/addProducts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
       },
-      body:JSON.stringify(Fakedata[0])
-  })
+      body: JSON.stringify(Fakedata[0])
+    });
+  };
   return (
     <div>
       <button onClick={handleAddProduct}>Add Product </button>
     </div>
   );
-}; 
+};
 
 export default Admin;
