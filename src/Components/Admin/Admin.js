1e6f2586ab488f1a6377d80156ca849879622b08
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-20 15:49:11 +0600

    ok connect server

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Fakedata from "../../Fakedata/fakedata.json";
 const Admin = () => {
+  const handleAddProduct = () => {
+    fetch("https://5562b.sse.codesandbox.io/addProducts",{
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(Fakedata[0])
+  })
   return (
     <div>
-      <h1>Admin is comming soon</h1>
+      <button onClick={handleAddProduct}>Add Product </button>
     </div>
   );
 };
